bc6fed87d855e49dec4d1b3b65950f9d7f99395a
Moises Oliveira Catonio de Araújo <moisesc@imd.local>
2019-11-16 14:38:22 -0300

    Identificando comandos de ex3, mas com problemas ao ler a formatação de column major

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
+import os
 
-f = open("column-major.asm", "r")
+#Removing the tabs;
+with open("exe3.asm", "r") as f, open("output1.asm", "w") as f2:
+	list_lines = f.readlines()
 
-total_chars_in_line = []
+	words = []
 
-flines = f.readlines()
+	for real_line in list_lines:
+		words.append(real_line.strip("	"))
+		f2.write(real_line.strip("	"))
+	
+	f.close()
+	f2.close()
 
-for line in flines:
-	lenght_of_line = len(line)
-	total_chars_in_line.append(lenght_of_line)
-	print(line)
+#Removing the empty lines;
+with open("output1.asm", "r") as f, open("output2.asm", "w") as f2:
+	for line in f:
+		if not line.strip(): continue
+		f2.write(line)
 
-f.close()
+	f.close()
+	f2.close()
 
-for lenght in total_chars_in_line:
-	print(lenght)
+#Reading the first words of each line, identifying and removing comments, sections and labels, writing in output_commands.asm after processing;
+with open("output2.asm", "r") as f, open("output_commands.asm", "w") as f2:
+	for line in f:
+		first_word = line.split(None, 1)[0]
+		if line[0] == "." or line[0] == "#": continue
+		if first_word[-1] == ":": continue
+		f2.write(first_word+"\n")
 
-print("")
-print("")
-print("")
+	f.close()
+	f2.close()
 
-print(len(total_chars_in_line))
+os.remove("output1.asm")
+os.remove("output2.asm")
